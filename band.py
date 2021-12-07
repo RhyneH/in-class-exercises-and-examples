7a4a76e0be2f6664a27fb0d78baba82476552d69
Rhyne Horne <rhyneallen0628@gmail.com>
2021-12-07 16:52:44 -0500

    Merge branch 'main' of https://github.com/RhyneH/in-class-exercises-and-examples

---
+++ band.py
@@ -0,0 +1,7 @@
+bands_dict = {     
+    'Jimmy Eats World': {'members': 5},
+    'Queen': {'members': 4},
+    }   
+bands_dict["Mac Miller"] = {'members': 1}
+#print(f'The band "Queeen" has {bands_dict["Queen"]["members"]} members')
+print(f'The band "Jimmy Eats World" has {bands_dict["Jimmy Eats World"]["members"]} members')
